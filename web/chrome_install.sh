3e1ab117d64b3192647d1d93c820a2031f4f0c3b
importdavid <wilkidav@gmail.com>
2020-01-06 10:34:47 -0500

    changed apt to apt-get in chrome_install.sh

--- web/chrome_install.sh
+++ web/chrome_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-apt update
-apt install unzip
+apt-get update
+apt-get install unzip
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-apt install -y google-chrome-stable
+apt-get install -y google-chrome-stable
 wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 mv chromedriver /usr/bin/chromedriver
